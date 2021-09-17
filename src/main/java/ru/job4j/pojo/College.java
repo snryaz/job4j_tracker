6e46c469f7f5e7fa415d0d94ef2c737f30ff5c57
sryaz <snryazancev@gmail.com>
2021-09-18 00:28:55 +0300

    2. Модель данных. [#174683] Задание

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Ryazantsev Sergey Nikolaevich");
+        student.setGroup(1);
+        student.setAdmission(new Date());
+
+        System.out.println("Name: " + student.getFio() + ", group: " + student.getGroup() + ", Date of admission: " + student.getAdmission());
+    }
+}
