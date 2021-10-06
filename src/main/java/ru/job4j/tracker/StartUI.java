eb6a3f6d63a574ee156a81bac25d75266a829309
neggoGH <neggo1985@mail.ru>
2021-10-06 12:14:48 +0300

    2.5. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -28,7 +28,19 @@ public class StartUI {
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
                 }
-            } else {
+            } else if (select == 2) {
+                System.out.println("=== Edit item ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                if (tracker.replace(id, item)) {
+                    System.out.println("Заявка заменена успешно");
+                } else {
+                    System.out.println("Ошибка замены заявки");
+                }
+            } else if (select == 6) {
                 run = false;
             }
         }
