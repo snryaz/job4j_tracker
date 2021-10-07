cecaf5124986890cbf2f8fa5eb3143a3a8c6a0f5
neggoGH <neggo1985@mail.ru>
2021-10-07 11:55:49 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int count) {
+
+    }
+
+    @Override
+    public double refuel(double fuelQuan) {
+        return 0;
+    }
+}
