7ad0703a3f08f1c5d01e3db7321e89efeccad8c3
neggoGH <neggo1985@mail.ru>
2021-10-14 23:13:46 +0300

    7. Шаблон проектирования - Стратегия [#786]

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -1,6 +1,7 @@
 package ru.job4j.strategy;
 
 public class Square implements Shape {
+
     @Override
     public String draw() {
         String ln = System.lineSeparator();
--- src/main/java/ru/job4j/strategy/Triangle.java
+++ src/main/java/ru/job4j/strategy/Triangle.java
@@ -1,6 +1,7 @@
 package ru.job4j.strategy;
 
 public class Triangle implements Shape {
+
     @Override
     public String draw() {
         String ln = System.lineSeparator();
