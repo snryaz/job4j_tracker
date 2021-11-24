1db298e1aee970c0f9b2629bf22f2feb41d143cb
neggoGH <neggo1985@mail.ru>
2021-11-24 22:38:11 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #232167]

--- src/test/java/ru/job4j/collection/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/collection/PhoneDictionaryTest.java
@@ -1,9 +1,12 @@
 package ru.job4j.collection;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class PhoneDictionaryTest {
@@ -16,5 +19,15 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenNotFind() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Ivan");
+        assertEquals(persons.isEmpty(), true);
     }
 }
