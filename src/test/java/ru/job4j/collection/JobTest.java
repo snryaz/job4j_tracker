f3a5999535c699d94c300d6470d6857b75f9ac6d
neggoGH <neggo1985@mail.ru>
2022-01-19 21:21:50 +0300

    2. Комбинированный компаратор [#10036 #252602]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -14,9 +14,9 @@ public class JobTest {
                 .thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Impl task", 1)
         );
-        assertThat(rsl, lessThan(0));
+        assertThat(rsl, greaterThan(0));
     }
 
     @Test
